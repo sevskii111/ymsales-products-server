f6bc4e8691e3b7319d27481ee8017fbdaf53b1f0
sevskii111 <sevskii111@users.noreply.github.com>
2021-01-28 17:48:27 +0300

    updated codes

--- listConverter.js
+++ listConverter.js
@@ -6,15 +6,27 @@ const lines = [...new Set(list.split("\n").map((l) => l.trim()))];
 fs.writeFileSync("./list.txt", lines.join("\n"));
 console.log(lines.length);
 
+let addedNames = {};
+
 let resultMap = {};
 
 for (const line of lines) {
-  const [link, _, code] = line.split("\t");
+  const [link, name, code] = line.split("\t");
   const id = link.match(/\d+/)[0];
 
   if (!resultMap[code]) {
     const discount = parseInt(code.match(/\d+/)[0]) / 100;
     resultMap[code] = { code, discount, products: new Set() };
+  }
+  if (addedNames[name]) {
+    //console.log(`${name} is duped`);
+    if (addedNames[name].length < id.length) {
+      resultMap[code].products.delete(addedNames[name]);
+    } else {
+      continue;
+    }
+  } else {
+    addedNames[name] = id;
   }
   resultMap[code].products.add(id);
 }
